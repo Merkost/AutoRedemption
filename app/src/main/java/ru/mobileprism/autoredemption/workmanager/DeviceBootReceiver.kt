39a5103818645fba277df011d21dd4a384682f54
Konstantin Merenkov <56008710+Merkost@users.noreply.github.com>
2022-09-17 21:44:20 +1000

    Settings improving

--- app/src/main/java/ru/mobileprism/autoredemption/workmanager/DeviceBootReceiver.kt
+++ app/src/main/java/ru/mobileprism/autoredemption/workmanager/DeviceBootReceiver.kt
@@ -5,14 +5,15 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import ru.mobileprism.autoredemption.ForegroundService
+import ru.mobileprism.autoredemption.startSmsService
 
 class DeviceBootReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         Log.v("log_tag", "Action :: "+intent.action);
         if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
-           // context.startSmsService()
-            val serviceIntent = Intent(context, ForegroundService::class.java)
-            context.startService(serviceIntent)
+            context.startSmsService()
+            /*val serviceIntent = Intent(context, ForegroundService::class.java)
+            context.startService(serviceIntent)*/
         }
     }
 }
